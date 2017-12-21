ed53304823fb6304616b8127bab72055be700b0a
shuliu <shuliu.gardiana@gmail.com>
2017-12-22 00:30:46 +0800

    Metronome add 2 method

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 import { Metronome } from './classes/Metronome';
 
 (function(){
-  let myMetronome = new Metronome();
+  let myOption = {
+    elements: document.querySelectorAll('.box')
+  };
+  let myMetronome = new Metronome(myOption);
   console.log('init app');
 })();
--- js/classes/Metronome.js
+++ js/classes/Metronome.js
@@ -1,7 +1,49 @@
 
+const defaultOptions = {
+  /** @param {object} elements 要監聽回傳的元素 */
+  elements: []
+};
+
+/**
+ * class Metronome
+ */
 export class Metronome {
 
-  constructor(options = []) {
+  /**
+   * constructor
+   * @param {json} myOptions 
+   */
+  constructor(myOptions = {}) {
     console.log('new a Metronome');
+
+    /**
+     * 選項
+     * TODO: 尚未做 myOptions 內多餘選項的過濾
+     */
+    this.options = Object.assign(defaultOptions, myOptions);
+
+    this.initial();
+  }
+
+  /**
+   * setEventListener
+   * 加入監聽
+   * @param {NodeList} element 
+   */
+  setEventListener(element) {
+    console.log('setEventListener');
+    console.log(element);
+  }
+
+  /**
+   * initial
+   * private method
+   */
+  initial() {
+    console.log('initial');
+    
+    this.options.elements.forEach(element => {
+      this.setEventListener(element);
+    });
   }
 }
